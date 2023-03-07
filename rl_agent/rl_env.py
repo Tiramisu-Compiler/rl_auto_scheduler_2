df8fb9652b2ba6d02f90c1cd62f2a7220b32984b
djamelrassem <id_lamouri@esi.dz>
2023-03-07 16:25:27 +0400

    added binary based reward

--- rl_agent/rl_env.py
+++ rl_agent/rl_env.py
@@ -1,4 +1,4 @@
-import random, math, numpy as np
+import random, numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 from env_api.tiramisu_api import TiramisuEnvAPIv1
@@ -12,7 +12,7 @@ class TiramisuRlEnv(gym.Env):
         self.tiramisu_api = TiramisuEnvAPIv1()
         # Define action and observation spaces
         self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(-np.inf,np.inf,shape=(180,))
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(180,))
         self.reset()
 
     def reset(self, seed=None, options={}):
@@ -30,7 +30,6 @@ class TiramisuRlEnv(gym.Env):
         self.done = self.truncated = False
         self.info = {}
         return self.state, self.info
-        # return obs
 
     def step(self, action):
         match action:
@@ -38,11 +37,47 @@ class TiramisuRlEnv(gym.Env):
                 speedup, embedded_tensor, legality = self.tiramisu_api.parallelize(0)
                 if legality:
                     self.state = embedded_tensor.numpy()
-                    self.reward = speedup
-                else:
-                    self.reward = 0.5
             case 1:
                 # Exit case
-                self.reward = 1.0
+                speedup , legality = (1.0 , True)
+
+        self.reward = self.calculate_reward(speedup=speedup,legality=legality)
         self.done = True
-        return self.state, math.log(self.reward,1.5), self.done, self.truncated, self.info
+
+        return self.state, self.reward, self.done, self.truncated, self.info
+
+    def calculate_reward(self,speedup: float, legality: bool):
+        if legality : 
+            if speedup > 1 :
+                reward = 10
+            if(speedup == 1):
+                reward = -5
+            else :
+                reward = 10
+        else : 
+            reward = -10
+        return reward
+
+'''
+SeparateNN-binary_reward :
+        if legality : 
+            if speedup >= 1 :
+                reward = 10
+            else :
+                reward = -10
+        else : 
+            reward = 0
+
+SeparateNN-binary_reward_exit_punish:
+        if legality : 
+            if speedup > 1 :
+                reward = 10
+            if(speedup == 1):
+                reward = -5
+            else :
+                reward = -10
+        else : 
+            reward = 0
+
+
+'''
